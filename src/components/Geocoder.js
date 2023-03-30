660b32595b5f01ffa77b8f1c2ff0773cd00e768c
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-03-30 13:54:24 +0530

    geocoder modified

--- src/components/Geocoder.js
+++ src/components/Geocoder.js
@@ -2,18 +2,18 @@ import MapBoxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import { useControl } from 'react-map-gl';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
-const Geocoder = (props) => { 
+const Geocoder = (props) => {
   const ctrl = new MapBoxGeocoder({
-    accessToken:"pk.eyJ1IjoicmFzaGlkcmFzaGkiLCJhIjoiY2xmbm5yazJqMDEyNjN1cW15bW16aHZyZyJ9.z8umjVwkkrxiAoVlKMeHOw"  ,
+    accessToken:
+      'pk.eyJ1IjoicmFzaGlkcmFzaGkiLCJhIjoiY2xmbm5yazJqMDEyNjN1cW15bW16aHZyZyJ9.z8umjVwkkrxiAoVlKMeHOw',
     // marker: false,
     collapsed: true,
   });
   useControl(() => ctrl);
   ctrl.on('result', (e) => {
     const coords = e.result.geometry.coordinates;
-    props.handilePin(coords)
-     console.log(coords);
-
+    props.handilePin(coords);
+    console.log(coords);
   });
   return null;
 };
