10ed4cb40691b673051f893b9be7ca2d726718fb
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-03-30 13:50:55 +0530

    adminDashboard data fetched

--- src/pages/admin/Dashboard/AdminDashboard.js
+++ src/pages/admin/Dashboard/AdminDashboard.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AdminHeader from '../../../components/header/AdminHeader';
 import '../../../style/adminDashboard.css';
 import Footer from '../../../components/footer/Footer';
 import Chart from '../../../components/admin -components/Chart';
+import axios from '../../../axios/axios';
+import { toast } from 'react-hot-toast';
 const AdminDashboard = () => {
+   const [dashboardData,setDashboardData]=useState({})
+  useEffect(()=>{
+    try{
+    axios.get("/admin/get-dashboard", {
+      headers: { 'x-access-admintoken': localStorage.getItem('admintoken') },
+    }).then((response)=>{
+      if(response.data.status === 'success'){
+        setDashboardData(response.data)
+      }else{
+        toast.error(response.data.message)
+      }
+      console.log(response.data);
+    })}catch(error){
+      toast.error(error.message)
+    }
+  },[])
   return (
     <div>
       <div className="">
@@ -37,25 +55,25 @@ const AdminDashboard = () => {
             <div className="chart mt-4 ">
               <div className="  badgeButton ">
                 <div className="notification-content">
-                  <h1>$00</h1>
+                  <h1>+{dashboardData.todayBookingCount}</h1>
                   <smal>Today Bookings</smal>
                 </div>
               </div>
               <div className="  badgeButton">
                 <div className="notification-content">
-                  <h1>$00</h1>
+                  <h1>+{dashboardData.todayTestDriveCount}</h1>
                   <smal>Today Test Drive Bookings</smal>
                 </div>
               </div>
               <div className="  badgeButton">
                 <div className="notification-content">
-                  <h1>$00</h1>
+                    <h1>$ {dashboardData.revenue}</h1>
                   <smal>Total Revenue</smal>
                 </div>
               </div>
               <div className="  badgeButton">
                 <div className="notification-content">
-                  <h1>$00</h1>
+                  <h1>+{dashboardData.totalBooking}</h1>
                   <smal>Total Bookings</smal>
                 </div>
               </div>
@@ -73,7 +91,7 @@ const AdminDashboard = () => {
           </div>
         </div>
       </div>
-          <Footer />
+      <Footer />
     </div>
   );
 };
