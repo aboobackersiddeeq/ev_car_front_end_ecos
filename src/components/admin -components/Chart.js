65f7042a756a8647718696433da1aafb719f7668
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-03-30 13:58:55 +0530

    unwanted data deleted

--- src/components/admin -components/Chart.js
+++ src/components/admin -components/Chart.js
@@ -31,16 +31,7 @@ const Chart = () => {
       toast.error(error.message)
     }
   },[])
-  const data01 = [
-    { name: 'Group A', value: 400 },
-    { name: 'Group B', value: 300 },
-    { name: 'Group C', value: 300 },
-    { name: 'Group D', value: 200 },
-    { name: 'Group E', value: 400 },
-    { name: 'Group F', value: 300 },
-    { name: 'Group G', value: 300 },
-    { name: 'Group D', value: 200 },
-  ];
+  
   // const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
   return (
     <div style={{ textAlign: 'center' }}>
