7dc3df6c0db71589898ef46b30b5215805c44888
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-04-01 13:09:04 +0530

    file  modification

--- src/components/Geocoder.js
+++ src/components/Geocoder.js
@@ -4,8 +4,7 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 const Geocoder = (props) => {
   const ctrl = new MapBoxGeocoder({
-    accessToken:
-      'pk.eyJ1IjoicmFzaGlkcmFzaGkiLCJhIjoiY2xmbm5yazJqMDEyNjN1cW15bW16aHZyZyJ9.z8umjVwkkrxiAoVlKMeHOw',
+    accessToken:process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
     // marker: false,
     collapsed: true,
   });
