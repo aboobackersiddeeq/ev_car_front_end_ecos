4156fd11e3209ccb655ff491fba3fd9bdfaf5aba
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-04-06 09:28:44 +0530

    loader page

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../style/loader.css';
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
