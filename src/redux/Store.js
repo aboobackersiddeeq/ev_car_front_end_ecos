792846ac50cb7187076202fdf4b24ed71c6ca27b
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-04-06 09:32:44 +0530

    persistor added

---
+++ src/redux/Store.js
@@ -0,0 +1,47 @@
+// import { configureStore } from '@reduxjs/toolkit';
+
+// import adminReducer from './Admin';
+// import dealerReducer from './Dealer';
+// import productReducer from './Product';
+// import userReducer from './User';
+//  export const store = configureStore({
+//   reducer: {
+//     admin: adminReducer,
+//     dealer: dealerReducer,
+//     product: productReducer,
+//     user: userReducer,
+//   },
+// });
+
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // change to a storage engine of your choice
+
+import adminReducer from './Admin';
+import dealerReducer from './Dealer';
+import productReducer from './Product';
+import userReducer from './User';
+import groupReducer from './Community';
+import booleanReducer from './Boolean';
+
+const rootReducer = combineReducers({
+  admin: adminReducer,
+  dealer: dealerReducer,
+  product: productReducer,
+  user: userReducer,
+  group: groupReducer,
+  boolean: booleanReducer,
+});
+const persistConfig = {
+  key: 'root', // this is the key used to store the persisted state in storage
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+const persistor = persistStore(store);
+export { store, persistor };
