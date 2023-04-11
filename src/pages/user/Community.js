6adbebc616948d86de0372e0f446624258278b48
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-04-11 18:00:37 +0530

    updation

--- src/pages/user/Community.js
+++ src/pages/user/Community.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ChatMain from '../../components/chatcomponents/ChatMain';
-import Sidebar from '../../components/chatcomponents/Sidebar';
+import ChatMain from '../../components/chat-components/ChatMain';
+import Sidebar from '../../components/chat-components/Sidebar';
 import '../../style/community.css';
 import { io } from 'socket.io-client';
+import Profile from '../../components/user-components/Profile';
+import { useState } from 'react';
 const Community = () => {
   const socket = io('http://localhost:3001');
+  const [profileShow, setProfileShow] = useState(false);
   return (
-    <div>
-      <div className="app_community">
-        <div className="app_body_community">
-          <Sidebar />
-          <ChatMain socket={socket} />
-        </div>
+    <div className="app_community">
+      <div className="app_body_community">
+        <Sidebar />
+        {profileShow && <Profile setProfileShow={setProfileShow} />}
+        <ChatMain socket={socket} setProfileShow={setProfileShow} />
       </div>
     </div>
   );
