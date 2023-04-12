9c719fabbf3e9a6e5f818633da2114ced1adb992
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-04-12 18:14:58 +0530

    error fix

--- src/redux/admin.js
+++ src/redux/admin.js
@@ -8,6 +8,8 @@ export const adminSlice = createSlice({
       state.value = action.payload;
     },
   },
+
+  
 });
 export const { adminlogin } = adminSlice.actions;
 export default adminSlice.reducer;
