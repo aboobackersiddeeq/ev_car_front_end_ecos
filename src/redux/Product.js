0e769cdbf109eb818e09c6aab4ddd994ce36ebc1
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-04-12 19:22:00 +0530

    product js file added

---
+++ src/redux/Product.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const productSlice = createSlice({
+  name: 'productDetails',
+  initialState: { value: {} },
+  reducers: {
+    product: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+export const { product } = productSlice.actions;
+export default productSlice.reducer;
