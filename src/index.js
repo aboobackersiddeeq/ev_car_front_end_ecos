a97635faa8a66a05d1a78de7e3580178ffba0582
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-04-13 14:45:51 +0530

    code clean

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import '../src/style/index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/Store';
-
 import { BrowserRouter } from 'react-router-dom';
 import { auth, provider, app, db } from './firebase/Firebase-config';
 import { PersistGate } from 'redux-persist/integration/react';
 import { createContext } from 'react';
-const firebaseContext =createContext()  
+const firebaseContext = createContext();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
