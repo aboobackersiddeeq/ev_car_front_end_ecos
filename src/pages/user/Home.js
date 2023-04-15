80b0d8c5fcb63fc2853e0222dd5c081ca9b2dfee
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-04-15 17:44:41 +0530

    video popup added

--- src/pages/user/Home.js
+++ src/pages/user/Home.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeaderTwo from '../../components/header/HeaderTwo';
 import '../../style/home.css';
+import VideoPopupModal from '../../components/user-components/VideoPopupModal';
 
 const Home = () => {
+  const [isOpen, setOpen] = useState(false);
   return (
     <>
       <HeaderTwo />
-      <div>
+      <div className="home-parent">
         <img className="nexonEv" src="../../../Images/nexonEv.jpg" alt="" />
+
+        <img
+          onClick={() => setOpen(true)}
+          className="nexonEv"
+          src="../../../Images/nexon-ev-max-video.jpg"
+          alt=""
+        />
+
+        <VideoPopupModal setOpen={setOpen} isOpen={isOpen} />
+        <img
+          className="nexonEv"
+          src="../../../Images/PeerformanceMax.png"
+          alt=""
+        />
       </div>
     </>
   );
