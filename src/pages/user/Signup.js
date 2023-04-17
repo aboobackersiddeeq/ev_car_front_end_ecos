fe95a49e6382282dac77ecac0e0cbd294dfccfab
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-04-17 12:46:16 +0530

    Signup validation updated

--- src/pages/user/Signup.js
+++ src/pages/user/Signup.js
@@ -16,36 +16,76 @@ function Signup() {
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
-
+  const [formErrors, setFormErrors] = useState({});
   const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (password === '' || phone === '' || email === '' || name === '') {
-      swal('sorry!', 'All fields are required!', 'error');
-    } else {
-      try {
-        axios
-          .post('/signup', {
-            username: name,
-            email: email,
-            password: password,
-            phone,
-          })
-          .then((response) => {
-            if (response.data.status === 'success') {
-              swal('SUCCESS', response.data.message, 'success');
-              localStorage.setItem('usertoken', response.data.token);
-              setUserLoginStatus(true);
-              dispatch(userData(response.data.result));
-              navigate('/');
-            } else {
-              swal('OOPS', response.data.message, 'error');
-            }
-          });
-      } catch (error) {
-        toast(error.message);
+    const errors = validate();
+    if (Object.keys(errors).length === 0) {
+      setFormErrors(errors);
+      if (password === '' || phone === '' || email === '' || name === '') {
+        swal('sorry!', 'All fields are required!', 'error');
+      } else {
+        try {
+          axios
+            .post('/signup', {
+              username: name,
+              email: email,
+              password: password,
+              phone,
+            })
+            .then((response) => {
+              if (response.data.status === 'success') {
+                swal('SUCCESS', response.data.message, 'success');
+                localStorage.setItem('usertoken', response.data.token);
+                setUserLoginStatus(true);
+                dispatch(userData(response.data.result));
+                navigate('/');
+              } else {
+                swal('OOPS', response.data.message, 'error');
+              }
+            });
+        } catch (error) {
+          toast(error.message);
+        }
       }
+    } else {
+      setFormErrors(errors);
     }
+  };
+
+  const validate = () => {
+    const phoneRegex = /^\d{10}$/;
+    const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
+    const passwordRegex = /^\d{6}$/;
+    const errors = {};
+    if (!name) {
+      errors.name = 'Name is required';
+    } else if (!name.trim()) {
+      errors.name = 'Name is required';
+    } else if (!nameRegex.test(name)) {
+      errors.name = 'Please enter a valid name';
+    }
+    if (!email) {
+      errors.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = 'Email is invalid';
+    }
+    if (!phone) {
+      errors.phone = 'Phone number is required';
+    } else if (!phone.trim()) {
+      errors.phone = 'Phone is required';
+    } else if (!phoneRegex.test(phone)) {
+      errors.phone = 'Please enter a valid 10-digit phone number.';
+    }
+    if (!password) {
+      errors.password = 'Password is required';
+    } else if (!password.trim()) {
+      errors.password = 'Password is required';
+    } else if (!passwordRegex.test(password)) {
+      errors.password = 'Please enter at least six characters';
+    }
+    return errors;
   };
   return (
     <div>
@@ -69,26 +109,27 @@ function Signup() {
             value={name}
             onChange={(e) => setName(e.target.value)}
             id="fname"
-            name="name"
-            required={true}
             placeholder="Username"
-            defaultValue="John"
           />
+          {formErrors.name && (
+            <span className=" msg_signup">{formErrors.name}</span>
+          )}
           <br />
           <label htmlFor="lname">Phone</label>
           <br />
           <input
             className="input"
-            type="number"
+            type="text"
             value={phone}
-            required={true}
+            // required={true}
             minLength={10}
             onChange={(e) => setPhone(e.target.value)}
             id="lname"
-            name="phone"
             placeholder="Ph:8137020393"
-            defaultValue="Phone"
           />
+          {formErrors.phone && (
+            <span className="  msg_signup">{formErrors.phone}</span>
+          )}
           <br />
           <label htmlFor="fname">Email</label>
           <br />
@@ -98,11 +139,11 @@ function Signup() {
             id="fname"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            name="email"
             placeholder="Email"
-            required={true}
-            defaultValue="John"
           />
+          {formErrors.email && (
+            <span className="  msg_signup">{formErrors.email}</span>
+          )}
           <br />
           <label htmlFor="lname">Password</label>
           <br />
@@ -112,17 +153,23 @@ function Signup() {
             id="lname"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            name="password"
-            required={true}
             placeholder="Password"
             minLength={6}
           />
+          {formErrors.password && (
+            <span className="  msg_signup">{formErrors.password}</span>
+          )}
           <br />
           <br />
           <button className="loginButton">Login</button>
         </form>
 
-        <span onClick={() => navigate('/login')}>Login</span>
+        <span
+          style={{ fontSize: 'medium', marginTop: '40px' }}
+          onClick={() => navigate('/login')}
+        >
+          Sign in instead
+        </span>
       </div>
     </div>
   );
